8d7e7c175da728bb61ee1c88c0ccbbcbe80384c9
Zaid Hasan <you@example.com>
2022-12-10 21:13:40 +0530

    ok da

--- fd.cpp
+++ fd.cpp
@@ -4,26 +4,34 @@ using namespace std;
 
 class fd
 {
-    float p,r,t,a;
+    public:
+    double p,r,t,ma,in,n;
     public:
     fd() // default constructor
     {
-        p=0.0,r=0.0;t=0.0,a=0.0;
-        
+        cin>>p;
+        cin>>r;
+        cin>>t;
+        cin>>n;
     }
-    void read()
+    
+    fd(fd & f1) //copy constructor and a parameterized passing an object by reference 
     {
-        cout<<"Enter the values of amount ,rate of interest, time in years "
+        f1.ma=f1.p*(pow((1+(0.01*f1.r/f1.n)),f1.n*f1.t));
+        cout<< "Maturity amount is: "<<f1.ma<<endl;
     }
-    fd(fd f1) //parameterized constructor passing an object
+    fd(double a,double b,double c,double d)
     {
-
+        cout<<"Interest earned is: "<<(a-b)<<"\nRate of interest per period is: "<<(c/d)<<endl;
     }
 
     
 };
 int main()
 {
-
+    cout<<"Enter the values of amount ,rate of interest, time in years and number of compounding frequency per year (n) :"<<endl;
+    fd f1;
+    fd f2(f1);
+    fd f3(f1.ma,f1.p,f1.r,f1.n);
     return 1;
 }
